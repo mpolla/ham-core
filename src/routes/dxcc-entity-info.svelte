81d776c50ae7a4b20c6743415dec70a55ae88c50
Jakob Kordež <jakobkordez1999@gmail.com>
2024-06-27 16:43:45 +0200

    Update dxcc info styling

--- src/routes/dxcc-entity-info.svelte
+++ src/routes/dxcc-entity-info.svelte
@@ -5,6 +5,8 @@
 	export let dxccEntity: DxccEntity;
 
 	$: tz = dxccEntity.timez;
+	$: lat = dxccEntity.lat;
+	$: long = dxccEntity.long;
 </script>
 
 <div>
@@ -12,20 +14,40 @@
 	<div>{dxccEntity.name}</div>
 </div>
 <table class="mx-auto w-auto">
-	<tr><td>CQ Zone:</td><td>{dxccEntity.cqz ?? '?'}</td></tr>
-	<tr><td>ITU Zone:</td><td>{dxccEntity.ituz ?? '?'}</td></tr>
-	<tr><td>Continent:</td><td>{dxccEntity.cont ?? '?'}</td></tr>
+	<tr><td>CQ Zone</td><td>{dxccEntity.cqz ?? '?'}</td></tr>
+	<tr><td>ITU Zone</td><td>{dxccEntity.ituz ?? '?'}</td></tr>
+	<tr><td>Continent</td><td>{dxccEntity.cont ?? '?'}</td></tr>
 </table>
 <table class="mx-auto w-auto">
-	<tr><td>Latitude:</td><td>{dxccEntity.lat ?? '?'}</td></tr>
-	<tr><td>Longitude:</td><td>{dxccEntity.long ?? '?'}</td></tr>
 	<tr>
-		<td>TZ Offset:</td>
-		{#if tz !== undefined}
-			<td>{tz >= 0 ? '+' : ''}{tz}h</td>
-		{:else}
-			<td>?</td>
-		{/if}
+		<td>Latitude</td>
+		<td>
+			{#if lat !== undefined}
+				{Math.abs(lat)}°{lat >= 0 ? 'N' : 'S'}
+			{:else}
+				?
+			{/if}
+		</td>
+	</tr>
+	<tr>
+		<td>Longitude</td>
+		<td>
+			{#if long !== undefined}
+				{Math.abs(long)}°{long >= 0 ? 'W' : 'E'}
+			{:else}
+				?
+			{/if}
+		</td>
+	</tr>
+	<tr>
+		<td>TZ Offset</td>
+		<td>
+			{#if tz !== undefined}
+				{tz >= 0 ? '+' : '-'}{Math.abs(tz).toFixed(2).padStart(5, '0').replace('.', ':')}
+			{:else}
+				?
+			{/if}
+		</td>
 	</tr>
 </table>
 
