90d98b7a77ef7df719a7bcbc46bb807fed77956b
Jakob Kordež <jakobkordez1999@gmail.com>
2024-07-11 13:25:11 +0200

    Add supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,8 @@
+import { PUBLIC_SUPABASE_ANON, PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { createClient } from '@supabase/supabase-js';
+import type { Database } from './database.types';
+
+export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON);
+
+export type Profile = Database['public']['Tables']['profile']['Row'];
+export type Qso = Database['public']['Tables']['qso']['Row'];
