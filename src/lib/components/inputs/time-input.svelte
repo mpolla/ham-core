b01df5eff21be1e1718513a7bbc2613c5a00fda4
Jakob Kordež <jakobkordez1999@gmail.com>
2024-11-05 19:03:08 +0100

    Fix time input

--- src/lib/components/inputs/time-input.svelte
+++ src/lib/components/inputs/time-input.svelte
@@ -4,11 +4,11 @@
 	export let label: string;
 
 	function onInput() {
-		const val = t.value;
+		let val = t.value;
 		const sel = val.length - (t.selectionStart ?? 0);
-		t.value = val.replaceAll(/\D/g, '');
-		t.value = t.value.substring(t.value.length - 4, t.value.length).padStart(4, '0');
-		t.setSelectionRange(4 - sel, 4 - sel);
+		val = val.replaceAll(/\D/g, '');
+		t.value = val.substring(val.length - 4, val.length).padStart(4, '0');
+		setTimeout(() => t.setSelectionRange(4 - sel, 4 - sel), 0);
 	}
 </script>
 
@@ -22,7 +22,6 @@
 		e.preventDefault();
 		t.value = t.value.substring(0, sStart) + e.key + t.value.substring(sEnd + 1);
 		t.setSelectionRange(sStart + 1, sStart + 1);
-		onInput();
 		t.dispatchEvent(new Event('input'));
 	}}
 	on:input={onInput}
