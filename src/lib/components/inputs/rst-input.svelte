7c3c71ad8cdd13d1f09f29061a62762b4dba5832
Jakob Kordež <jakobkordez1999@gmail.com>
2024-11-26 17:54:54 +0100

    Improve RST input field

--- src/lib/components/inputs/rst-input.svelte
+++ src/lib/components/inputs/rst-input.svelte
@@ -9,12 +9,14 @@
 
 	let { value = $bindable(''), label = '', mode = undefined, ...rest }: Props = $props();
 
-	let [defaultValue, defaultStartSel, defaultEndSel] = $derived(
+	const [defaultValue, defaultStartSel, defaultEndSel] = $derived(
 		((): [string, number, number] | [] => {
 			switch (mode) {
 				case 'SSB':
 				case 'LSB':
 				case 'USB':
+				case 'FM':
+				case 'AM':
 					return ['59', 1, 2];
 				case 'CW':
 				case 'RTTY':
@@ -25,19 +27,20 @@
 		})()
 	);
 
-	let canFillDefault = $derived(
-		defaultValue !== undefined && (value === '' || value === defaultValue)
+	const canFill = $derived(defaultValue && value === '');
+	const canSelect = $derived(
+		defaultValue &&
+			defaultValue.substring(0, defaultStartSel) === value.substring(0, defaultStartSel) &&
+			defaultValue.substring(defaultEndSel ?? 0) === value.substring(defaultEndSel ?? 0)
 	);
 </script>
 
 <input
 	type="text"
 	onfocus={(e) => {
-		if (!canFillDefault) return;
 		const field = e.currentTarget;
-		field.value = defaultValue ?? '';
-		field.setSelectionRange(defaultStartSel ?? 0, defaultEndSel ?? 0);
-		value = defaultValue ?? '';
+		if (canFill) field.value = defaultValue ?? '';
+		if (canSelect) field.setSelectionRange(defaultStartSel ?? 0, defaultEndSel ?? 0);
 	}}
 	bind:value
 	placeholder={label}
