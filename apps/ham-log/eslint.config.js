b2305722a3a7c02c6f0619e629b01d96f7beea4a
Jakob Kordež <jakobkordez1999@gmail.com>
2025-01-04 13:55:53 +0100

    Add .vercel to eslint ignore

--- apps/ham-log/eslint.config.js
+++ apps/ham-log/eslint.config.js
@@ -28,6 +28,6 @@ export default [
 		}
 	},
 	{
-		ignores: ['build/', '.svelte-kit/', 'dist/', 'src/lib/data/geo/', 'src-tauri/']
+		ignores: ['build/', '.svelte-kit/', 'dist/', 'src/lib/data/geo/', 'src-tauri/', '.vercel/']
 	}
 ];
